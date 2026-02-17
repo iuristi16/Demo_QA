4a3cd13c92b4758739c928dca4891a19ac6451ce
iuristi16 <153536563+iuristi16@users.noreply.github.com>
2026-02-17 22:13:26 +0400

    Complete DemoQA Practice Form automation base test

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -14,7 +14,7 @@ public class BaseTest {
         playwright = Playwright.create();
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
-                        .setHeadless(false)
+                        .setHeadless(true)
                         .setArgs(List.of("--start-maximized"))
         );
 
